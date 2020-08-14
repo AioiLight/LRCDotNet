561436cdf2f311b2c397a5cacc1b90c83af742de
AioiLight <info@aioilight.space>
2020-08-15 02:20:05 +0900

    remove unused test

--- LRCDotNetTest/UnitTest1.cs
+++ LRCDotNetTest/UnitTest1.cs
@@ -6,13 +6,6 @@ namespace LRCDotNetTest
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
-        {
-            System.Diagnostics.Debugger.Launch();
-            var s = LRCDotNet.Parse(File.ReadAllText(@"C:\Dev\Koioto\Build\Songs\Test\Haruiro-Gemination.lrc"));
-        }
-
         [Theory]
         [InlineData("[01:02:22]‰ÌŽŒ", "‰ÌŽŒ")]
         [InlineData("[01:02:22][01:02:22]‰ÌŽŒ", "‰ÌŽŒ")]
