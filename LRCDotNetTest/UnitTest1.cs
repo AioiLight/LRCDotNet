d5767c5af2ab080cacc0e2771d61344d175ef8fc
AioiLight <info@aioilight.space>
2021-07-23 05:20:51 +0900

    +テスト

--- LRCDotNetTest/UnitTest1.cs
+++ LRCDotNetTest/UnitTest1.cs
@@ -1,4 +1,3 @@
-using System.IO;
 using Space.AioiLight.LRCDotNet;
 using Xunit;
 
@@ -16,6 +15,7 @@ namespace LRCDotNetTest
 
         [Theory]
         [InlineData("[01:02:22]‰ÌŽŒ", "[01:02:22]")]
+        [InlineData("[01:02.22]‰ÌŽŒ", "[01:02.22]")]
         [InlineData("[01:02:22][01:03:22]‰ÌŽŒ", "[01:02:22]")]
         [InlineData("–³Ž‹[01:02:22][01:03:22]‰ÌŽŒ", "[01:02:22]")]
         public void GetTimeString(string x, string y)
